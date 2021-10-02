d0d9b38c252c8ff50fc10abf97d03b79d722899c
Kat Adams Shannon <katashannon@gmail.com>
2021-10-01 18:28:17 -0700

    completed some practice with tidyverse

--- Tidyverse.R
+++ Tidyverse.R
@@ -1,1 +1,34 @@
 #### Tidyverse Practice ####
+
+
+library(tidyverse)
+library(DT)
+
+#regular
+mean(mtcars$mpg)
+
+#tidy
+mtcars$mpg %>% mean()
+
+#add function
+gpm <- function (mpg) {1/mpg} #check this code for function
+
+#regular
+round(mean(gpm(mtcars$mpg)), digits = 2)
+
+#tidy
+gpm(mtcars$mpg) %>% 
+  mean() # how to round digits
+
+################tutorial for tidyverse
+
+#read in csv file
+sgf <- read_csv("stiller_scales_data.csv")
+
+#check out data
+view(sgf)
+
+DT::datatable(sgf)
+
+
+
